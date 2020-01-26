cd946f793c0b9df6edfe9a926eeb8442222f8ce0
Ahmed OUMHELLA <40814810+ahmedoumhella@users.noreply.github.com>
2020-01-26 19:48:50 +0100

    @RepositoryRestResource

--- src/main/java/org/sid/cinema/dao/CinemaRepository.java
+++ src/main/java/org/sid/cinema/dao/CinemaRepository.java
@@ -2,6 +2,8 @@ package org.sid.cinema.dao;
 
 import org.sid.cinema.entities.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
 public interface CinemaRepository extends JpaRepository<Cinema,Long> {
 
 }
